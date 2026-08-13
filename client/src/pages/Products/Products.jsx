8c95bae8cf6c9aa47258c0d9cb54ec9c06356204
shuklapramudit <shuklapramudit287@gmail.com>
2026-08-13 12:05:17 +0530

    Base URL UPDATE

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -29,10 +29,12 @@ import "./Products.css";
 
 import API_BASE_URL from "../../services/api.js";
 
-const API =
-  "https://inventry-management-system-k9a5.onrender.com/api/products";
+const API = `${API_BASE_URL}/products`;
 
-const SERVER_URL =  "https://inventry-management-system-k9a5.onrender.com";
+const SERVER_URL = API_BASE_URL.replace(/\/api\/?$/, "");
+
+const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
+const MAX_IMAGES = 10;
 
 const ALLOWED_IMAGE_TYPES = [
   "image/jpeg",
