02b5f62eef8655aa2abe70dd817fa51854879216
shuklapramudit <shuklapramudit287@gmail.com>
2026-08-13 10:44:29 +0530

    Base URL UPDATE

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "https://inventry-management-system-k9a5.onrender.com/api";
+const API_BASE_URL =
+  "https://inventry-management-system-k9a5.onrender.com/api";
 
 export default API_BASE_URL;
